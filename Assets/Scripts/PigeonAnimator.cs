e2cc1adde60ef91c1e99d4d6b90fca8b1f1156bc
Tom Turner <tom.turner2@googlemail.com>
2018-01-27 10:06:37 +0000

    Animated bird boi

---
+++ Assets/Scripts/PigeonAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PigeonAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private bool is_gliding = false;
+    private float next_state_change = 0;
+
+    [SerializeField] Vector2 flap_range = new Vector2(1, 3);
+    [SerializeField] Vector2 glide_range = new Vector2(1, 3);
+
+
+    // Use this for initialization
+    void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	}
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.time >= next_state_change)
+        {
+            is_gliding = !is_gliding;
+            animator.SetBool("is_gliding", is_gliding);
+
+            float random_delay = is_gliding ? Random.Range(glide_range.x, glide_range.y) : Random.Range(flap_range.x, flap_range.y);
+            next_state_change = Time.time + random_delay;
+        }
+	}
+}
