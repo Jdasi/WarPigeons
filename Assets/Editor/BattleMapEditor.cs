0bc563d97a66cc7708b3ddf28690f0a8da0ffc10
Connor Rhone <connor2.rhone@live.uwe.ac.uk>
2018-01-27 00:05:59 +0000

    Added BattleMap scene

---
+++ Assets/Editor/BattleMapEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BattleMap))]
+public class BattleMapEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		if (GUILayout.Button ("Advance")) {
+			BattleMap variable = (BattleMap)target;
+			variable.Advance ();
+		}
+		if (GUILayout.Button ("Retreat")) {
+			BattleMap variable = (BattleMap)target;
+			variable.Retreat ();
+		}
+	}
+
+}
