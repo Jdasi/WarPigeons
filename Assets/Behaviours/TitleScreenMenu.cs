ebe75b615b18aae3900ebd1143131537c410f4b4
Tom Turner <tom.turner2@googlemail.com>
2018-01-28 13:11:31 +0000

    Fixed load bug

--- Assets/Behaviours/TitleScreenMenu.cs
+++ Assets/Behaviours/TitleScreenMenu.cs
@@ -6,14 +6,16 @@ using UnityEngine.SceneManagement;
 public class TitleScreenMenu : MonoBehaviour
 {
     [SerializeField] private int next_scene_build_index = 1;
+    private bool loading = false;
 
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetButton("Controller 1 - A"))
+        if (Input.GetButton("Controller 1 - A") && loading == false)
         {
             SceneManager.LoadSceneAsync(next_scene_build_index);
+            loading = true;
         }
 	}
 }
